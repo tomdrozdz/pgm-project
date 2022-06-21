6fb12193674b962fce4a0463ec8c5f3d1bc8a0f3
Dawid Motyka <dawidmt@gmail.com>
2022-06-21 13:47:26 +0200

    saving results to files; added exemplary results

--- lstm_evaluation.py
+++ lstm_evaluation.py
@@ -1,3 +1,5 @@
+import sys
+
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
@@ -9,7 +11,8 @@ from src.lstm import get_best_param_model
 import torch
 import numpy as np
 
-PARAMS_TUNING_RESULTS_FILE = 'hp_tuning_df.pkl'
+OUT_TO_FILE = True
+PARAMS_TUNING_RESULTS_FILE = 'hp_tuning_2.pkl'
 
 torch.manual_seed(11)
 np.random.seed(11)
@@ -18,6 +21,7 @@ np.random.seed(11)
 def evaluate_mcdlstm(df, regression, n_splits=4):
     f1_vals = []
     mse_vals = []
+    clf_reports = []
     f1_vals_train = []
     mse_vals_train = []
     for i in range(0, n_splits):
@@ -37,25 +41,33 @@ def evaluate_mcdlstm(df, regression, n_splits=4):
 
         plt.figure(figsize=(14, 8))
         if regression:
-            mse, f1 = visualise_regr_results(model, x_test[0:10], y_test[0:10], device=device, show_charts=True)
+            mse, f1, clf_report = visualise_regr_results(model, x_test, y_test, device=device, show_charts=True)
         else:
-            mse, f1 = visualise_clf_results(model, x_test[0:10], y_test[0:10], device=device, show_charts=True)
+            mse, f1, clf_report = visualise_clf_results(model, x_test, y_test, device=device, show_charts=True)
         plt.grid()
         plt.title(f'Predictions for split {i}')
         plt.xlabel('Test sample')
         model_type = 'regr' if regression else 'clf'
-        plt.savefig(f'predictions_{model_type}_split{i}.png')
+        plt.savefig(f'results/lstm/predictions_{model_type}_split{i}.png')
         f1_vals.append(f1)
         mse_vals.append(mse)
+        clf_reports.append(clf_report)
 
-    return f1_vals, mse_vals, f1_vals_train, mse_vals_train
+    return f1_vals, mse_vals, clf_reports, f1_vals_train, mse_vals_train
 
 
 df = load_data()
-f1_vals_regr, mse_vals_regr, f1_vals_train_regr, mse_vals_train_regr = evaluate_mcdlstm(df, True)
-f1_vals_clf, mse_vals_clf, f1_vals_train_clf, mse_vals_train_clf = evaluate_mcdlstm(df, False)
+f1_vals_regr, mse_vals_regr, clf_reports_regr, f1_vals_train_regr, mse_vals_train_regr = evaluate_mcdlstm(df, True)
+f1_vals_clf, mse_vals_clf, clf_reports_clf, f1_vals_train_clf, mse_vals_train_clf = evaluate_mcdlstm(df, False)
+
+
+if OUT_TO_FILE:
+    sys.stdout = open('results/lstm/evaluation.txt', "w")
 
 print(f'Regression without MCD: MSE: {np.mean(mse_vals_train_regr):.3f} ({np.std(mse_vals_train_regr):.3f}), f1: {np.mean(f1_vals_train_regr):.3f}({np.std(f1_vals_train_regr):.3f})')
 print(f'Regression with MCD: MSE: {np.mean(mse_vals_regr):.3f} ({np.std(mse_vals_regr):.3f}), f1: {np.mean(f1_vals_regr):.3f}({np.std(f1_vals_regr):.3f})')
 print(f'Classifier without MCD: MSE: {np.mean(mse_vals_clf):.3f} ({np.std(mse_vals_clf):.3f}), f1: {np.mean(f1_vals_clf):.3f}({np.std(f1_vals_clf):.3f})')
 print(f'Classifier with  MCD: MSE: {np.mean(mse_vals_train_clf):.3f} ({np.std(mse_vals_train_clf):.3f}), f1: {np.mean(f1_vals_train_clf):.3f}({np.std(f1_vals_train_clf):.3f})')
+
+print('\n Classification report for classification model, fold 3')
+print(clf_reports_clf[2])
--- lstm_hparams.py
+++ lstm_hparams.py
@@ -1,19 +1,29 @@
 import pickle
 
+import numpy as np
 import pandas as pd
+import torch
 
 from src.data import load_data
 from src.lstm import hp_tuning
 
-FIRST_TUNING_FILE = 'hp_tuning_df.pkl'
-SECOND_TUNING_FILE = 'hp_tuning_all.pkl'
+FIRST_TUNING_FILE = 'hp_tuning_1.pkl'
+SECOND_TUNING_FILE = 'hp_tuning_2.pkl'
+
+MAX_EPOCHS = 300
+
+torch.manual_seed(11)
+np.random.seed(11)
+
+device = 'cuda:0'
 
 df = load_data()
 
-param_grid_1 = {'sequence_length': [5, 10, 15], 'no_layers': [2, 3, 4], 'hidden_dim': [16,32,64], 'regression': [True, False],
+param_grid_1 = {'sequence_length': [5, 10, 15], 'no_layers': [2, 3, 4], 'hidden_dim': [16, 32, 64], 'regression': [True, False],
                 'drop_prob': [0.5], 'lr': [0.0001]}
 
-#res_df_lr = hp_tuning(df, param_grid_1, FIRST_TUNING_FILE, epochs=1)
+save_dir = 'models/lstm'
+res_df_lr = hp_tuning(df, param_grid_1, FIRST_TUNING_FILE, device=device, save_dir=save_dir, epochs=MAX_EPOCHS)
 
 
 def tune_drop_lr(regression):
@@ -28,18 +38,27 @@ def tune_drop_lr(regression):
                   'lr': [0.0001],
                   'drop_prob': [0.25, 0.1]}
     model_type = 'regr' if regression else 'clf'
-    res_df_drop = hp_tuning(df, param_grid, f'hp_tuning_{model_type}_drop.pkl')
+    res_drop_file = f'hp_tuning_{model_type}_drop.pkl'
+    _ = hp_tuning(df, param_grid, res_drop_file, device=device, save_dir=save_dir, epochs=MAX_EPOCHS)
 
     param_grid['drop_prob'] = [0.5]
     param_grid['lr'] = [0.003, 0.001]
-    res_df_lr = hp_tuning(df, param_grid, f'hp_tuning_{model_type}_lr.pkl')
+    res_lr_file = f'hp_tuning_{model_type}_lr.pkl'
+    _ = hp_tuning(df, param_grid, res_lr_file, device=device, save_dir=save_dir, epochs=MAX_EPOCHS)
 
-    return res_df_drop, res_df_lr
+    return res_drop_file, res_lr_file
 
 
-res_df_regr_drop, res_df_regr_lr = tune_drop_lr(regression=True)
-res_df_clf_drop, res_df_clf_lr = tune_drop_lr(regression=False)
+res_drop_file_regr, res_lr_file_regr = tune_drop_lr(regression=True)
+res_drop_file_clf, res_lr_file_clf = tune_drop_lr(regression=False)
 
-res_all = pd.concat((res_df_regr_drop, res_df_regr_lr, res_df_clf_drop, res_df_clf_lr), axis=0, ignore_index=True)
+
+res_dfs = []
+for filename in (FIRST_TUNING_FILE, res_drop_file_regr, res_lr_file_regr, res_drop_file_clf, res_lr_file_clf):
+    with open(filename, 'rb') as f:
+        res_dfs.append(pickle.load(file=f))
+
+
+res_all = pd.concat(res_dfs, axis=0, ignore_index=True)
 with open(SECOND_TUNING_FILE, 'wb') as f:
     pickle.dump(obj=res_all, file=f)
